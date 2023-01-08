4550175b4350cc7352a9cbeeca12d7ce8b2ee42c
ViniciusCabral <cabral.vm@outlook.com>
2023-01-08 01:15:10 -0300

    Study on project planning, markup and text editing in SwiftUI

---
+++ CodePlanning/CodePlanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePlanningApp.swift
+//  CodePlanning
+//
+//  Created by Vinicius Cabral on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CodePlanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
